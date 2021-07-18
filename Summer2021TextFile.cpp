574b40538b21e19add650464069fba51ca371ace
kevinzhang1 <30486411+kevinzhang1@users.noreply.github.com>
2021-07-18 14:13:45 -0400

    Update Summer2021TextFile.cpp

--- Summer2021TextFile.cpp
+++ Summer2021TextFile.cpp
@@ -3,15 +3,17 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 int main() {
 	fstream file; //creates a fstream variable
 	file.open("file.txt", ios::in); //opens file.txt
-	if (!file) {
-		cout << "Open file failed"; //if file variable doesn't exist, print this message
-	}
+	if (!file) cout << "Open file failed"; //if file variable doesn't exist, print this message
 	else {
-		cout << "Open file succeeded"; //otherwise, print this message
+		cout << "Open file succeeded\n"; //otherwise, print this message
+		string tp;
+		while (getline(file, tp)) //read data from file and put it into string.
+			cout << tp << "\n"; //prints the string
 		file.close(); //closes the file
 	}
 	return 0;
